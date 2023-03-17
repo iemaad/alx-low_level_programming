9e04ae751b5860c6cd98169f3aeeb0f30d0bf6b8
iemaad <oummad.meriem@gmail.com>
2023-03-16 19:49:17 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main(void)
-{
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+int main(void){
+/** 
+* Description: main function
+* Return :  if  no errors
+*/
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
