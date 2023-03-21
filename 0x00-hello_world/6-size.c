0aed6350f68fbe27fcae27d4ead52f07088317e0
iemaad <oummad.meriem@gmail.com>
2023-03-20 19:34:09 -0700

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@
 int main(void)
 
 {
-	ptintf("size of a char: %zu byte(s)\n", sizeof(char));
+	ptintf("size of a char : %zu byte(s)\n", sizeof( char ));
         ptintf("size of an int : %zu byte(s)\n", sizeof(int));
 	ptintf("size of a long int : %zu byte(s)\n", sizeof(long int));
 	ptintf("size of a long long int : %zu byte(s)\n", sizeof(long long int));
