262c19d9b982c291bc6aa47a9f683b807627f086
iemaad <oummad.meriem@gmail.com>
2023-03-20 18:53:19 -0700

    h

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints  the size of various types 
+ * on the computer it is compiled and run on
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	ptintf("size of a char: %zu byte(s)\n", sizeof(char));
+        ptintf("size of an int : %zu byte(s)\n", sizeof(int));
+	ptintf("size of a long int : %zu byte(s)\n", sizeof(long int));
+	ptintf("size of a long long int : %zu byte(s)\n", sizeof(long long int));
+	ptintf("size of a float : %zu byte(s)\n", sizeof(float));
+	return (0);
+
+}
