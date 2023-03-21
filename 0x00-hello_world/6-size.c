eb79dbc2559fdbd86a4c52be1ce1e174a4d25399
iemaad <oummad.meriem@gmail.com>
2023-03-20 19:29:05 -0700

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+
+
 /**
- * main - prints  the size of various types on the computer it is compiled and run on
+ * main - prints  the size of various types on
+ * the computer it is compiled and run on
  * Return: Always 0.
  */
 
 int main(void)
+
 {
 	ptintf("size of a char: %zu byte(s)\n", sizeof(char));
         ptintf("size of an int : %zu byte(s)\n", sizeof(int));
