5bd8c80a44a540a59691bea19b4ce03a03122282
iemaad <oummad.meriem@gmail.com>
2023-03-20 17:03:23 -0700

    hhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
+
 /** 
-* Description: main -Prints out a string to stdout.
-* Return :  if  success
-* 
-*/
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
+ * main -prints "Programming is like building a multilingual puzzle"
+ * return 0.
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0;
 }
