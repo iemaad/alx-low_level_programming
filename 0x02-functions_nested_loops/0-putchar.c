9a7dd428206f6d0fb59c68099bfeec5d886ffdc1
iemaad <oummad.meriem@gmail.com>
2023-03-22 18:57:53 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,26 +8,26 @@
 int main(void)
 {
 
-    _putchar('_');
+       _putchar('_');
 
-    _putchar('p');
+       _putchar('p');
     
-    _putchar('u');
+       _putchar('u');
     
-    _putchar('t');
+       _putchar('t');
     
-    _putchar('c');
+       _putchar('c');
     
-    _putchar('h');
+       _putchar('h');
     
-    _putchar('a');
-    
-    
-    _putchar('r');
+       _putchar('a');
     
     
-   _putchar('\n');
+       _putchar('r');
+    
+    
+       _putchar('\n');
    
-   return (0);
+       return (0);
 }
 
